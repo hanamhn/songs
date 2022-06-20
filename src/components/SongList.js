1a956c1727dff9d4fb04248d50b8bafb50ab4600
hanamhn <hanamhndev@gmail.com>
2022-06-20 13:36:29 +0700

    list song -redux basic

---
+++ src/components/SongList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { connect } from "react-redux";
+import { selectSong } from "../actions/";
+
+const SongList = (props) => {
+  const { songs, selectSong } = props;
+  // console.log(props);
+  return (
+    <div className="ui divided list">
+      {songs.map((song) => (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button
+              className="ui button primary"
+              onClick={() => selectSong(song)}
+            >
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  // console.log(state);
+  return { songs: state.songs };
+};
+
+export default connect(mapStateToProps, { selectSong })(SongList);
